0ff7e8aae60a5d02465b92e1d0f79c9d9cfac5ff
Zack Bloom <zackbloom@gmail.com>
2015-10-30 14:57:29 -0400

    Restructure

--- script.js
+++ script.js
@@ -1,26 +1,93 @@
-window.onload = function () {
-    var style, options;
-
-    options = INSTALL_OPTIONS;
-
-    style = document.createElement('style');
-    document.head.appendChild(style);
-
-    style.innerHTML = (
-        '.progressContainer{ position: fixed; ' + options.Position + ': 0; left: 0; width: 100%; height: ' + options.Height + 'px; background: ' + options.ProgressBarColorOptions.Background + '; }' +
-        '.progress{ height: ' + options.Height + 'px; background: ' + options.ProgressBarColorOptions.ProgressBar + '; width: 0; transition: width 0.5s; }'
-    );
-
-    document.body.innerHTML += '<div class="progressContainer"><div id="progress" class="progress"></div></div>';
-
-    function updateProgress(num1, num2) {
-        var percent = Math.ceil(num1 / num2 * 100) + '%';
-        document.getElementById('progress').style.width = percent;
+(function(){
+    if (!document.addEventListener ||
+        (  document.documentElement.style.transform === undefined
+        && document.documentElement.style.webkitTransform === undefined)){
+        return;
     }
 
-    window.addEventListener('scroll', function () {
+    var TRANSFORM = document.documentElement.style.webkitTransform !== undefined ? 'webkitTransform' : 'transform';
+
+    var options = INSTALL_OPTIONS;
+
+    var containerEl = null;
+    var barEl = null;
+    var render = function(){
+        if (!document.body)
+            return;
+
+        if (!containerEl){
+            containerEl = document.createElement('div');
+            document.body.appendChild(containerEl);
+
+            barEl = document.createElement('div');
+            barEl.className = 'scroll-progress-bar-bar';
+            containerEl.appendChild(barEl);
+        }
+
+        containerEl.className = 'scroll-progress-bar-container';
+        containerEl.className += ' scroll-progress-bar-' + options.position;
+
+        barEl.style.height = options.thickness + "px";
+        barEl.style.backgroundColor = options.barColor;
+
+        if (options.containerHasColor)
+            containerEl.style.backgroundColor = options.containerColor;
+        else
+            containerEl.style.backgroundColor = '';
+
+        updateProgress();
+    };
+
+    var now = function(){
+        return (window.performance && performance.now()) || (+ new Date);
+    };
+
+    var lastCall = 0;
+    var updateProgress = function(){
+        if (!barEl)
+            return;
+
+        // Don't allow updates faster than 60 FPS
+        if (now() - lastCall < 15)
+            return;
+
+        lastCall = now();
+
         var top = window.scrollY;
         var height = document.body.getBoundingClientRect().height - window.innerHeight;
-        updateProgress(top, height);
-    });
-}
+
+        var percent = Math.min(top / height * 100, 100);
+        barEl.style[TRANSFORM] = 'translateX(' + -(100 - percent) + '%)';
+
+        if (   (options.hideUntilScroll && percent < 5)
+            || (options.hideAfterDone && percent > 95)){
+            containerEl.style[TRANSFORM] = 'translateY(-' + options.thickness + 'px)';
+        } else {
+            containerEl.style[TRANSFORM] = '';
+        }
+
+        if (options.translucent){
+            containerEl.style.opacity = 0.9;
+        } else {
+            containerEl.style.opacity = 1;
+        }
+    };
+
+    var setOptions = function(opts){
+        options = opts;
+
+        render();
+    };
+
+    window.addEventListener('scroll', updateProgress);
+    window.addEventListener('resize', updateProgress);
+
+    if (document.readyState == 'loading')
+        window.addEventListener('DOMContentLoaded', render);
+    else
+        render();
+
+    window.ScrollProgressBar = {
+        setOptions: setOptions
+    };
+})();
